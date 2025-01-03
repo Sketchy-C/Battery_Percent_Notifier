853b063b5818b119cf4ddcaedfb02631543445f3
Sketchy-C <willy254dabs@gmail.com>
2025-01-03 06:09:19 -0800

    new changes

--- my_env/Battery_notify.py
+++ my_env/Battery_notify.py
@@ -2,6 +2,7 @@ import time
 import psutil
 from plyer import notification
 
+plugin = False
 
 battery = psutil.sensors_battery()
 percent = battery.percent
@@ -14,8 +15,20 @@ def popup(message):
     )
 
 
-if percent < 50:
-    popup("Less than 50\n"+str(percent)+"% Battery percent remaining")
-else:
-    popup("More than 50\n"+str(percent)+"% Battery percent remaining")
+# if percent < 50:
+#     popup("Less than 50\n"+str(percent)+"% Battery percent remaining")
+# else:
+#     popup("More than 50\n"+str(percent)+"% Battery percent remaining")
 
+def checker():
+
+    if percent > 80:
+        popup("Please plug out your charger\n"+str(percent)+"% Battery percent remaining")
+    else:
+        popup("Keep on charging\n"+str(percent)+"% Battery percent remaining")
+
+    time.sleep(300)
+    return checker()
+
+
+checker()
